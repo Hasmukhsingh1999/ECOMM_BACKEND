f1c7b36ed2cc369abee5b7a4234aa101a9b15b88
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-25 16:08:58 +0530

    initial commit

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 
@@ -24,26 +22,30 @@ const body = {
   context: [{ key: "autoselect_variant", value: true }],
 };
 
+const fetchData = async () => {
+  try {
+    const response = await axios.post(
+      "https://realtime.oxylabs.io/v1/queries",
+      body,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+            "Basic " + Buffer.from(`${username}:${password}`).toString("base64"),
+        },
+      }
+    );
 
-try {
-  const response = await axios.post(
-    "https://realtime.oxylabs.io/v1/queries",
-    body,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization:
-          "Basic " + Buffer.from(`${username}:${password}`).toString("base64"),
-      },
-    }
-  );
-
-  console.log(await response.data);
-} catch (error) {
-  console.log(error);
-}
+    console.log(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT,()=>{
-    console.log(`Server started at PORT : ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server started at PORT : ${PORT}`);
 });
+
+// Call the asynchronous function
+fetchData();
